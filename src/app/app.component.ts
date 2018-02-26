5d6657ea29a372e2bd1b3da2c51eae7be6c993cf
manoj <manojgolla93@gmail.com>
2018-02-25 20:07:54 -0600

    Modified already existed file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core'
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector:'app-root',
+    template:`<div>
+       <nav class="navbar navbar-default">
+        <div class="container-fluid">
+            <div class="navbar-header">
+            <a class="navbar-brand">Angular2</a>
+            </div>
+                <ul class="nav navbar-nav">
+                    <li class="active"><a [routerLink]="['/home']">Home</a></li>
+                    <li><a [routerLink]="['/products']">Products</a></li>
+                    <li><a  [routerLink]="['/order']">Orders</a></li>
+                    
+                </ul>
+        </div>
+        </nav>
+        <div class="container">
+            <router-outlet></router-outlet>
+        </div>
+    </div>`
 })
-export class AppComponent {
-  title = 'app';
+
+export class AppComponent{
+   
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,36 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser'
+import {FormsModule} from '@angular/forms';
+import {HttpModule} from '@angular/http';
+import {RouterModule} from '@angular/router'
 
-
-import { AppComponent } from './app.component';
-
-
+import {AppComponent} from './app.component'
+import { ManojComponent } from './manoj.component';
+import { NotFound } from './shared/notFound.component';
+import { HomeWork } from './home/home.component';
+import { ProductModule } from './products/product.module';
+import { OrderModule } from './orders/order.module';
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    imports:[
+        BrowserModule ,
+        FormsModule,
+        HttpModule,
+        ProductModule,
+        OrderModule,
+        RouterModule.forRoot([
+            {path:'home', component:HomeWork},
+            {path:'',redirectTo:'home',pathMatch:"full"},
+            {path:'**',component:NotFound}
+        ])
+    ],
+    declarations:[
+        AppComponent,
+        ManojComponent,
+        NotFound,
+        HomeWork
+    ],
+    bootstrap:[AppComponent]
+
 })
-export class AppModule { }
+
+export class AppModule{}
